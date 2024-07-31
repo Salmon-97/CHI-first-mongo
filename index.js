b75e6e0ad5a011bde7a2dc733f94a2b87bfe5afa
Your Name <you@example.com>
2024-07-31 14:15:19 +0100

    master

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { unknownRoute, getBase, createUser, fetchAllUsers, updateUser, deleteUser } = require("./controllers");
+const { unknownRoute, getBase, createUser, fetchAllUsers, updateUser, deleteUser, } = require("./controllers");
+const { validateSignupData, validateLoginData, } = require("./controllers/validators/auth.validators");
+const { loginUser, signupUser, isTokenValid } = require("./controllers/auth.controllers");
 const app = express();
 const PORT = 5454;
+const dotenv = require("dotenv");
+dotenv.config();
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
 app.get("/", getBase)
-app.post("/user", createUser);
-app.get("/User", fetchAllUsers);
+app.post("/user", validateSignupData, createUser);
+app.get("/User", fetchAllUsers,);
 app.put("/user/:id", updateUser)
-app.delete("/user/:id", deleteUser)
+app.post("/signup", validateSignupData, signupUser)
+app.post("/login", validateLoginData, loginUser);
+app.delete("/user/:id", deleteUser);
+app.post("/post", isTokenValid, (req, res) => {
+  try {
+    res.status(200).json({
+      message: "you are authorized to to view this page"
+  });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+            message: "server error"
+        });
+  }
+});
+
 app.all("*", unknownRoute);
 
 
 app.listen(PORT, async () => {
     try {
     console.log("server is running on port ${PORT}");
-    await mongoose.connect("mongodb://localhost:27017/first-mongo");
+    await mongoose.connect(process.env.DB_URL);
     console.log("db connected successfully");
   } catch (error) {
     console.log(error)
